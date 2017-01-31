59646e5dc55d3af853829c23c7342821234d791d
l3lackclevil <l3lackclevil@hotmail.com>
2017-01-31 15:01:36 +0700

    front end upload.php

--- upload.php
+++ upload.php
@@ -1,29 +1,152 @@
 <!DOCTYPE html>
 <html>
-<script type="text/javascript" src="js/jquery.js"></script>
-<link rel="stylesheet" href="css/css101.css">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXPassion : Be passioned by learning</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="img/foundation-icons/foundation-icons.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <link rel="stylesheet" href="css/css101.css">
+</head>
 <script type="text/javascript">
 var count = 0;
-$(function(){
-    $('p#add_field').click(function(){
+$(function() {
+    $('p#add_field').click(function() {
         count += 1;
         $('#container').append(
-                '<strong>Problem #' + count + '</strong><br />' 
-                + 'Joddd<br>' +'<textarea rows="4" cols="50" name="task[]"></textarea><br>' 
-                + 'A:' + '<input id="cA_' + count + '" name="choiceA[]' + '" type="text" />'
-                + 'B:' + '<input id="cB_' + count + '" name="choiceB[]' + '" type="text" />'
-                + 'C:' + '<input id="cC_' + count + '" name="choiceC[]' + '" type="text" />'
-                + 'D:' + '<input id="cD_' + count + '" name="choiceD[]' + '" type="text" />'
-                + 'Key:' + '<input id="key_' + count + '" name="key[]' + '" type="text" />'
-                + '<br>');
-    
+            '<strong>Problem #' + count + '</strong><br />' + 'ProblemContent<br>' + '<textarea rows="4" cols="50" name="task[]"></textarea><br>' + 'A:' + '<input id="cA_' + count + '" name="choiceA[]' + '" type="text" />' + 'B:' + '<input id="cB_' + count + '" name="choiceB[]' + '" type="text" />' + 'C:' + '<input id="cC_' + count + '" name="choiceC[]' + '" type="text" />' + 'D:' + '<input id="cD_' + count + '" name="choiceD[]' + '" type="text" />' + 'Key:' + '<input id="key_' + count + '" name="key[]' + '" type="text" />' + '<br>');
+
     });
 });
-</script> 
+</script>
 
 <body>
+    <!-- NAVIGATOR -->
+    <div data-sticky-container>
+        <div class="sticky" data-sticky data-options=" marginTop: 0; stickyOn: small;">
+            <div class="top-bar">
+                <div class="top-bar-left">
+                    <ul class="dropdown" data-dropdown-menu>
+                        <p class="menu-text"><img src="img/passion.png">&nbsp;&nbsp;<a href="index.php">EXPassion</a>
+                            <?php
+                                    if (isset($_SESSION['username']))
+                                    {
+                                    echo "<a class='dummy-nav-to'></a>
+                                          <a href='task.php' class='dummy-nav-to'>Task</a>
+                                          <a href='contest_list.php' class='nav-to'>Contest</a>";
+                                    }  
+                                ?>
+                        </p>
+                    </ul>
+                </div>
+                <div class="top-bar-right">
+                    <?php
 
-<?php
+                                if(!isset($_SESSION['username']))
+                                {
+                                    echo    "<ul class='menu menu-ld'><ul class='inline-list hide-for-small-only account-action'>
+                                                <li><a data-open='myModal'>Login</a></li>
+                                                <li><a data-open='regModal'>Signup</a></li>
+                                            </ul></ul>";
+                                }
+                                else if (isset($_SESSION['username']))
+                                {
+                                    echo "<ul class='menu-l menu inline-list'><ul class='inline-list hide-for-small-only account-action'>
+                                            <li class='islogged' data-open='infoModal'><span data-tooltip aria-haspopup='true' data-options='disable_for_touch:true' class='has-tip' title='Lv : Coin : Exp : '><a>".$_SESSION['username']."</a></span></li>
+                                            <li><span data-tooltip aria-haspopup='true' class='has-tip' data-disable-hover='false' tabindex='1' title='Logout' onclick='location=\"logout.php\"'><a><i class='fi-lock'></i></a></span></li>
+                                            </ul>;
+                                        </ul></ul>";
+                                }
+                            ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- REVEAL -->
+    <div id="myModal" class="large reveal" data-reveal aria-labelledby="login or sign up" aria-hidden="true" role="dialog" data-animation-in="fade-in" data-animation-out="fade-out">
+        <div class="row">
+            <div class="large-6 columns auth-plain">
+                <div class="signup-panel left-solid">
+                    <p class="welcome">Registered Users</p>
+                    <form name="form1" id="form" action="check_login.php" enctype="multipart/form-data" method="POST">
+                        <div class="row collapse">
+                            <div class="small-2  columns log-icon">
+                                <span class="prefix"><i class="fi-torso-female fi-lock-b"></i></span>
+                            </div>
+                            <div class="small-10  columns">
+                                <input type="text" name="username" id="username" placeholder="username">
+                            </div>
+                        </div>
+                        <div class="row collapse">
+                            <div class="small-2 columns log-icon">
+                                <span class="prefix"><i class="fi-lock fi-lock-b"></i></span>
+                            </div>
+                            <div class="small-10 columns ">
+                                <input type="password" name="password" id="password" placeholder="password">
+                            </div>
+                        </div>
+                        <button class="button" type="submit" name="submit">Log In </button>
+                    </form>
+                </div>
+            </div>
+            <div class="large-6 columns auth-plain">
+                <div class="signup-panel newusers">
+                    <p class="welcome"> New User?</p>
+                    <p>By creating an account with us, you will be able to move through the checkout process faster, view and track your orders, and more.</p>
+                    <br>
+                    <a class="button " data-open='regModal'>Sign Up</a></br>
+                </div>
+            </div>
+        </div>
+        <button class="close-button" data-close aria-label="Close Accessible Modal" type="button">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div id="regModal" class="large reveal" data-reveal aria-labelledby="Register" aria-hidden="true" data-animation-in="fade-in" data-animation-out="fade-out">
+        <div class="row expanded">
+            <h1 class="welcome">Register Todas</h1>
+            <div class="large-12 medium-12 small-12 large-centered columns">
+                <form name="regForm" id="form" action="in_regis.php" enctype="multipart/form-data" method="POST">
+                    <legend>Username</legend>
+                    <input type="text" name="username" id="username" placeholder="username" class="small-10">
+                    <legend>Password</legend>
+                    <input type="password" name="password" id="password" placeholder="password" class="small-10">
+                    <button class="button" type="submit" name="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+        <button class="close-button" data-close aria-label="Close Accessible Modal" type="button">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div id="infoModal" class="medium reveal" data-reveal aria-labelledby="info" aria-hidden="true" data-animation-in="fade-in" data-animation-out="fade-out">
+        <div class="row expanded">
+            <h1 class="welcome">Profile</h1>
+        </div>
+        <div class="row expanded">
+            <div class="large-12 medium-12 small-12 profile-container columns">
+                Username :
+                <br> Level :
+                <br> Exp :
+                <br> Coins :
+                <br>
+            </div>
+        </div>
+        <button class="close-button" data-close aria-label="Close Accessible Modal" type="button">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="upload-container">
+        <h1>New Contest</h1>
+        <div class="upload-content">
+            <?php
     //If form was submitted
     if (isset($_POST['btnSubmit'])) {
         //create instance of database class
@@ -68,16 +191,7 @@ $(function(){
                 $key = $_POST['key'][$i];
                 $i++;
                 /*//Insert into websites table
-                $sql_website = sprintf("INSERT INTO websites (Website_URL) VALUES ('%s')",
-                                       $db->real_escape_string($value) );  
-                $result_website = $db->query($sql_website);
-                $inserted_website_id = $db->insert_id;
-                
-                //Insert into users_websites_link table
-                $sql_users_website = sprintf("INSERT INTO users_websites_link (UserID, WebsiteID) VALUES ('%s','%s')",
-                                       $db->real_escape_string($inserted_user_id),
-                                       $db->real_escape_string($inserted_website_id) );  
-                $result_users_website = $db->query($sql_users_website);
+ 
                 */
                 $sql_task = "INSERT INTO task (subject, grouptask, task, choiceA, choiceB, choiceC, choiceD, checkAnswer)
                             VALUES ('$subject', '$inserted_contest_id', '$task', '$cA', '$cB', '$cC', '$cD', '$key')";
@@ -97,56 +211,37 @@ $(function(){
         $db->close();
     }
 ?>
-
-<?php if (!isset($_POST['btnSubmit'])) { ?>
-    <h1>New Contest</h1>
-    <form name="test" method="post" action="">
-        <label for="name">Contest:</label>
-        <input type="text" name="contest_name" id="contest" />
-        
-        <div class="spacer"></div>
-        
-        <label for="name">Subject:</label>
-        <select id="category-select" name="category">
-            <option value="">select subject</option>
-            <option value="0" >Mathemetics</option>
-            <option value="1" >English</option>
-            <option value="2" >Social</option>
-            <option value="3" >Physics</option>
-            <option value="4" >Chemistry</option>
-            <option value="5" >Biology</option>
-            <option value="6" >Thai</option>
-        </select>
-        
-        <div class="spacer"></div>
-    
-        <div id="container">
-            <p id="add_field"><a href="#"><span>&raquo; Add problems.....</span></a></p>
+                <?php if (!isset($_POST['btnSubmit'])) { ?>
+                <form name="test" method="post" action="">
+                    <label for="name">Contest:</label>
+                    <input type="text" name="contest_name" id="contest" />
+                    <div class="spacer"></div>
+                    <label for="name">Subject:</label>
+                    <select id="category-select" name="category">
+                        <option value="">select subject</option>
+                        <option value="0">Mathemetics</option>
+                        <option value="1">English</option>
+                        <option value="2">Social</option>
+                        <option value="3">Physics</option>
+                        <option value="4">Chemistry</option>
+                        <option value="5">Biology</option>
+                        <option value="6">Thai</option>
+                    </select>
+                    <div class="spacer"></div>
+                    <div class="form-margin">
+                    <div id="container">
+                        <p id="add_field"><a href="#"><span>&raquo; Add problems.....</span></a></p>
+                    </div>
+                    </div>
         </div>
-        
-        <div class="spacer"></div>
+        <div class="spacer "></div>
         <input id="go" name="btnSubmit" type="submit" value="Submit" class="btn" />
-    </form>
-<?php } ?>
-
-
-<!--
-<form action="addtask.php" method="post">
-    <p>Joddd</p>
-    <textarea rows="4" cols="50" name="Z"></textarea><br>
-    A: <input type="text" name="A"><br>
-    B: <input type="text" name="B"><br>
-    C: <input type="text" name="C"><br>
-    D: <input type="text" name="D"><br>
-    Key: <input type="text" name="key"><br>
-    <input type="submit">
- </form>
-*/
--->
-
-
-        <script src="js/vendor/what-input.js"></script>
-        <script src="js/vendor/foundation.js"></script>
-        <script src="js/app.js"></script>
+        </form>
+        <?php } ?>
+    </div>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
 </body>
+
 </html>
