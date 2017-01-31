3855cdcf69260ad8da0ec3aa92eaaaaf9966b8a8
l3lackclevil <l3lackclevil@hotmail.com>
2017-01-31 13:39:14 +0700

    task menu id ChooseSubject()

--- contest_list.php
+++ contest_list.php
@@ -153,13 +153,13 @@ include "contest_list_fetch.php";
             <div class="row expanded">
                 <div class="select-sbj expanded">
                     <div class="sbj">
-                        <div class="large-1 columns math "><a onclick="table(0)">Math</a></div>
-                        <div class="large-2 columns phys"><a onclick="table(1)">Physics</a></div>
-                        <div class="large-2 columns chem"><a onclick="table(2)">Chemistry</a></div>
-                        <div class="large-2 columns bio"><a  onclick="table(3)">Biology</a></div>
-                        <div class="large-2 columns eng"><a  onclick="table(4)">English</a></div>
-                        <div class="large-2 columns social"><a onclick="table(5)">Social Study</a></div>
-                        <div class="large-1 columns thai"><a onclick="table(6)">Thai</a></div>
+                        <div class="large-1 columns math "><a  onclick="ChooseSubject(0)">Math</a></div>
+                        <div class="large-2 columns phys"><a   onclick="ChooseSubject(1)">Physics</a></div>
+                        <div class="large-2 columns chem"><a   onclick="ChooseSubject(2)">Chemistry</a></div>
+                        <div class="large-2 columns bio"><a    onclick="ChooseSubject(3)">Biology</a></div>
+                        <div class="large-2 columns eng"><a    onclick="ChooseSubject(4)">English</a></div>
+                        <div class="large-2 columns social"><a onclick="ChooseSubject(5)">Social Study</a></div>
+                        <div class="large-1 columns thai"><a   onclick="ChooseSubject(6)">Thai</a></div>
                     </div>
                 </div>
             </div>
